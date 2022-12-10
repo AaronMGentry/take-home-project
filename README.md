05099bbde4b11a34d5d6449844524827f561102c
AaronMGentry <120216619+AaronMGentry@users.noreply.github.com>
2022-12-10 14:40:56 -0700

    set domain and default timeouts

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # take-home-project
 Test Plan and Automation project for Guild
+
+## Pre-reqs
+
+```bash
+npm
+```
--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+  "baseUrl" : "https://www.dropbox.com",
+  "defaultCommandTimeout": 5000,
+  "execTimeout": 60000,
+  "integrationFolder": "cypress/test",
+  "pageLoadTimeout": 300000000,
+  "requestTimeout": 5000,
+  "responseTimeout": 30000,
+  "screenshotsFolder": "cypress/screenshots",
+  "taskTimeout": 60000,
+  "viewportHeight": 1080, 
+  "viewportWidth": 1920
+  }
+})
